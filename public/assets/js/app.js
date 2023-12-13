67b08b6e163454b434c9d98cbb0150445f31f799
Kiyodo <tyler.odo@gmail.com>
2023-12-12 17:08:04 -0800

    fixed GET routes on app.js to route correctly + added uuid npm package for creating unique ids + added middleware for JSON and urlencoded + added POST route for /api/notes + added DELETE route for /api/notes/:note_id

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,26 +1,83 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const PORT = 3005;
 
-app.use(express.static('public'));
+const savedNotes = require('../../../db/db.json');
 
-app.get('/', (req, res) => {
-  // Figure out (test) why it's /public/ instead of /public
-  res.sendFile(path.join(__dirname, '/public/'));
-});
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../index.html'));
+}); */
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/notes.html'));
+  res.sendFile(path.join(__dirname, '../../notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-  // Check if this file path for db.json works. Might need to use ../db/db.json instead
-  res.sendFile(path.join(__dirname, '/db/db.json'));
+  res.json(savedNotes);
+  // res.sendFile(path.join(__dirname, '../../../db/db.json'));
+  res.status(200);
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body && title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4()
+    }
+  }
+
+  fs.readFile('../../../db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      parsedNotes.push(newNote);
+
+      fs.writeFile('../../../db/db.json', JSON.stringify(parsedNotes, null, 2), (writeErr) => console.error(writeErr));
+    }
+  });
+
+/*   const response = {
+    status: 'success',
+    body: newNote
+  }; */
+  res.status(201);
+});
+
+app.delete('/api/notes/:note_id', (req, res) => {
+  const reqDelNote = req.params.note_id;
+  
+  for (const i = 0; i < savedNotes.length; i++) {
+    if (reqDelNote === savedNotes[i].note_id) {
+      fs.readFile('../../db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+          parsedNotes.splice(i, 1);
+
+          fs.writeFile('../../../db/db.json', JSON.stringify(parsedNotes, null, 2), (writeErr) => console.error(writeErr));
+        }
+      });
+    }
+  }
+
+  // Need to find correct status code for DELETE route (probs not 200)
+  res.status(200);
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
 app.listen(PORT, () =>
